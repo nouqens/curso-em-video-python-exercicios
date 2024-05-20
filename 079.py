c061a54675d1b884e7366e4b73687b01f407b332
nouqens <gmpunisher490@gmail.com>
2024-05-20 19:34:48 -0300

    Atualizar o

---
+++ 079.py
@@ -0,0 +1,16 @@
+l = []
+c = True
+while c == True:
+    a = int(input('Digite um numero: '))
+    if a not in l:
+        l.append(a)
+        print('Valor adicionado com sucesso!')
+    else:
+        print('Valor Duplicado! Numero nao adicionado!')
+    b= input('Quer continuar? [s/n] ')
+    if b == 's':
+        c = True
+    else:
+        c = False
+l.sort()
+print(f'voce digitou os valores: {l}')
