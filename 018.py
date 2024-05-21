22f929eb17e3b87733c37a85561f513f953f57ab
nouqens <gmpunisher490@gmail.com>
2024-05-21 16:04:24 -0300

    Atualizar o

---
+++ 018.py
@@ -0,0 +1,6 @@
+from math import radians, sin, cos, tan
+an= float(input('um angulo: '))
+sin= sin(radians(an))
+cos= cos(radians(an))
+tan= tan(radians(an))
+print(f' o seno é {sin:.2f} \n o cosseno é {cos:.2f} \n a tangente é {tan:.2f}')
